97f19f7b94c2470a135bce1c1649af2e370d27fa
Sebas31121 <sebastian.carvajal110@misena.edu.co>
2022-11-24 21:20:23 -0500

    add function for update category etc

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path,include
-from .views import createCategoryView,createSubcategoryView,createProductView
+from .views import createCategoryView,createSubcategoryView,createProductView,editCategoryView,editSubcategoryView,editProductView
 urlpatterns = [
     path('inventory/new/category', createCategoryView, name='inventory_category' ),
     path('inventory/new/subcategory', createSubcategoryView, name='inventory_subcategory' ),
-    path('inventory/new/product', createProductView, name='inventory_product')
+    path('inventory/new/product', createProductView, name='inventory_product'),
+    path('inventory/<pk>/update/category', editCategoryView, name='inventory_update_category'),
+    path('inventory/<pk>/update/subcategory', editSubcategoryView, name='inventory_update_subcategory'),
+    path('inventory/<pk>/update/product', editProductView, name='inventory_update_product'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponseRedirect,get_object_or_404
 from .forms import CategoryForm,SubCategoryForm,ProductForm
+from .models import Category,SubCategory,Product
 def createCategoryView (request):
     template_name='inventory/inventory_form.html'
     form_inventory={}
@@ -12,6 +13,15 @@ def createCategoryView (request):
             form_inventory.save()
     return render(request,template_name,{'title':'SCJM-Crear Categoria','form':form_inventory})
 
+def editCategoryView (request,pk):
+    category = get_object_or_404(Category,id=pk)
+    categoryform = CategoryForm(request.POST or None,instance=category)
+    template_name = 'inventory/inventory_form.html'
+    if categoryform.is_valid():
+        categoryform.save()
+        return HttpResponseRedirect('/inventory/new/category')
+
+    return render(request,template_name,{'title':'SCJM-Actualizar Categoria','form':categoryform})
 def createSubcategoryView (request):
     template_name='inventory/inventory_form.html'
     form_inventory={}
@@ -24,6 +34,16 @@ def createSubcategoryView (request):
             form_inventory.save()
     return render(request,template_name,{'title':'SCJM-Crear Subcategoria','form':form_inventory})
 
+def editSubcategoryView (request,pk):
+    subcategory = get_object_or_404(SubCategory,id=pk)
+    subcategoryform = CategoryForm(request.POST or None,instance=subcategory)
+    template_name = 'inventory/inventory_form.html'
+    if subcategoryform.is_valid():
+        subcategoryform.save()
+        return HttpResponseRedirect('/inventory/new/subcategory')
+
+    return render(request,template_name,{'title':'SCJM-Actualizar Subcategoria','form':subcategoryform})
+
 def createProductView (request):
     template_name='inventory/inventory_form.html'
     form_inventory={}
@@ -35,3 +55,13 @@ def createProductView (request):
         if form_inventory.is_valid():
             form_inventory.save()
     return render(request,template_name,{'title':'SCJM-Crear Producto','form':form_inventory})
+
+def editProductView (request,pk):
+    product = get_object_or_404(Product,id=pk)
+    productform = CategoryForm(request.POST or None,instance=product)
+    template_name = 'inventory/inventory_form.html'
+    if productform.is_valid():
+        productform.save()
+        return HttpResponseRedirect('/inventory/new/product')
+
+    return render(request,template_name,{'title':'SCJM-Actualizar Producto','form':productform})
