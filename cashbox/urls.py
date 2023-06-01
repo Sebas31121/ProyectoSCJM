287602f9d6d4b1a33325eff3694ab33fa173a3fe
ApacheD2301 <dmurciabuitrago@gmail.com>
2023-06-01 11:53:26 -0500

    Changes

--- cashbox/urls.py
+++ cashbox/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import cashbox
+from .views import cashbox, tablesViews
+
 
 urlpatterns = [
     path( 'caja/', cashbox, name="cashbox"),
+    path('table/list', tablesViews.as_view(), name='table_list'),
+
 ]
--- cashbox/views.py
+++ cashbox/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
+from .models import Table
 
-# Create your views here.
 @login_required(login_url='/accounts/login/')
 def cashbox(request):
     return render(request,'cashbox/caja.html')
+
+def listar_mesas(request):
+    all_tables = Table.objects.all()
+    return render(request, 'cashbox/caja.html', {'all_tables': all_tables})
+
+class tablesViews(ListView):
+    template_name = "cashbox/caja.html"
+    model = Table
+    context_object_name = "tables"
+    """queryset = Table.objects.filter(is_active=True)"""
