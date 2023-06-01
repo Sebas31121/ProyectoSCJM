52c6f2c5f57f8661a912422ce6b0dbefaa8f788c
scastrillone <santiagocastrillon.ep@gmail.com>
2023-06-01 12:05:53 -0500

    fix import in cashbox

--- cashbox/urls.py
+++ cashbox/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from .views import cashbox, tablesViews
-
+from .views import cashbox
 
 urlpatterns = [
     path( 'caja/', cashbox, name="cashbox"),
-    path('table/list', tablesViews.as_view(), name='table_list'),
 
 ]
