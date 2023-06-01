acb10406d25f35443feb78d8fc648206d11bcc84
scastrillone <santiagocastrillon.ep@gmail.com>
2023-06-01 11:59:32 -0500

    List tables in cashbox template

--- cashbox/views.py
+++ cashbox/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from table.models import Mesa
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def cashbox(request):
-    return render(request,'cashbox/caja.html')
+    context = []
+    tables = Mesa.objects.all()
+    #get total price per table
+    return render(request,'cashbox/caja.html',{'tables':tables})
