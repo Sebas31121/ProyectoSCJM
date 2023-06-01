00287daa0f9df4a3d8034624971527d797751372
scastrillone <santiagocastrillon.ep@gmail.com>
2023-06-01 12:02:13 -0500

    merge with last changes

--- cashbox/views.py
+++ cashbox/views.py
@@ -6,21 +6,7 @@ from .models import Table
 
 @login_required(login_url='/accounts/login/')
 def cashbox(request):
-<<<<<<< HEAD
     context = []
     tables = Mesa.objects.all()
     #get total price per table
     return render(request,'cashbox/caja.html',{'tables':tables})
-=======
-    return render(request,'cashbox/caja.html')
-
-def listar_mesas(request):
-    all_tables = Table.objects.all()
-    return render(request, 'cashbox/caja.html', {'all_tables': all_tables})
-
-class tablesViews(ListView):
-    template_name = "cashbox/caja.html"
-    model = Table
-    context_object_name = "tables"
-    """queryset = Table.objects.filter(is_active=True)"""
->>>>>>> refs/remotes/origin/main
