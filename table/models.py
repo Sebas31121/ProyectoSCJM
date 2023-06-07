6781c7588285208ce934d79e69eebc9daa5d1776
ApacheD2301 <dmurciabuitrago@gmail.com>
2023-06-07 16:27:17 -0500

    Avanceeees :3

--- table/models.py
+++ table/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Mesa(models.Model):
     nro_mesa = models.IntegerField("Número de la mesa", help_text="Mesa",unique=True)
-    cant_sillas = models.IntegerField("Cantidad de sillas", help_text="Sillas", default=0)
+    cant_sillas = models.IntegerField("Cantidad de sillas", help_text="Sillas", default=4)
     is_active = models.BooleanField(default=True)
     def __str__(self) -> str:
         return f"{self.nro_mesa} - {self.cant_sillas}"
